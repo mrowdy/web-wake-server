92551c8e142dd86c5d92896d8ee8d897e5853bbc
Slemgrim <slemgrim@gmail.com>
2013-08-16 13:52:16 +0200

    show info if no clients registered

--- web-wake-server/templates/classic/classic.php
+++ web-wake-server/templates/classic/classic.php
@@ -11,14 +11,19 @@
     <title>Wakeup</title>
 </head>
 <body>
-<form id="wakeupForm" method="post" action="index.php">
+<?php if(count($this->getSleepers()) == 0): ?>
+    <span>Sorry, no clients registered</span>
+<?php else: ?>
+    <form id="wakeupForm" method="post" action="index.php">
     <?php foreach($this->getSleepers() as $sleeperKey => $sleeperName): ?>
         <button class="submit" type="button" name="sleeper" value="<?php echo $sleeperKey; ?>">wakeup <?php echo $sleeperKey; ?></button>
     <?php endforeach; ?>
-    <input type="hidden" name="csrf" value="<?php echo $this->csrfSave->getCSRF() ?>" />
-    <input id="sleeper" type="hidden" name="sleeper" value="" />
-    <input type="hidden" name="action" value="send-view" />
-</form>
+        <input type="hidden" name="csrf" value="<?php echo $this->csrfSave->getCSRF() ?>" />
+        <input id="sleeper" type="hidden" name="sleeper" value="" />
+        <input type="hidden" name="action" value="send-view" />
+    </form>
+<?php endif;?>
+
 <script type="text/javascript" src="/web-wake-server/templates/classic/classic.js"></script>
 </body>
 </html>
