6b07d7aafa4f73bc43adb00ac714b27ad35bc9fd
Slemgrim <slemgrim@gmail.com>
2013-08-16 13:47:52 +0200

    remove sleepers from config ( now clientside)

--- config-sample.php
+++ config-sample.php
@@ -1,11 +1,6 @@
 <?php
 
 $config = array(
-    'sleepers' => array(
-        'johndoe' => 'John Doe',
-        'janedoe' => 'Jane Doe',
-    ),
-
     //has to be writeable
     'status-file' => 'status.json',
     'template' => 'classic',
