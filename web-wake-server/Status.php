68308c4fce671d62b2a5e776697763b59ed21042
Slemgrim <slemgrim@gmail.com>
2013-08-16 13:06:54 +0200

    status class

---
+++ web-wake-server/Status.php
@@ -0,0 +1,106 @@
+<?php
+
+class Status {
+
+    /**
+     * @var array List of entities to wake
+     */
+    public $sleepers = array();
+
+    /**
+     * @var int timestamp of next wakeup (set by client)
+     */
+    public $nextWakeup = 0;
+
+    /**
+     * @var string path to status file
+     */
+    protected $filePath = '';
+
+    public function __construct($filePath){
+        $this->filePath = $filePath;
+        $this->checkFile();
+        $this->loadFromFile();
+    }
+
+    /**
+     * save object to file
+     */
+    public function save(){
+        $this->saveToFile();
+    }
+
+    /**
+     * populate this from file
+     */
+    public function load(){
+        $this->loadFromFile();
+    }
+
+    /**
+     * check if file exist or creates it
+     */
+    protected function checkFile(){
+        if(!$this->fileExists()){
+            $this->createFile();
+        }
+        $this->isWriteable();
+    }
+
+    /**
+     * @return bool file exists
+     */
+    protected function fileExists(){
+        if(file_exists($this->filePath)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @throws Exception create file
+     */
+    protected function createFile(){
+        $fp = @fopen($this->filePath, "w");
+        if($fp){
+            fwrite($fp, '');
+            fclose($fp);
+        } else {
+            throw new Exception('can\'t create file');
+        }
+    }
+
+    /**
+     * @return bool file is writeable
+     * @throws Exception
+     */
+    protected function isWriteable(){
+        if(is_writeable($this->filePath)){
+            return true;
+        } else {
+            throw new Exception('can\'t write to file');
+        }
+    }
+
+    /**
+     * save to file
+     */
+    protected function saveToFile(){
+        $json = json_encode($this);
+        file_put_contents($this->filePath, $json);
+    }
+
+    /**
+     * populate this from file
+     */
+    protected function loadFromFile(){
+        $data = json_decode(file_get_contents($this->filePath), true);
+        if(isset($data['sleepers'])){
+            $this->sleepers =$data['sleepers'];
+        }
+
+        if(isset($data['nextWakeup'])){
+            $this->nextWakeup = $data['nextWakeup'];
+        }
+    }
+}
---
+++ web-wake-server/tests/StatusTest.php
@@ -0,0 +1,114 @@
+<?php
+
+require_once '../Status.php';
+
+class StatusTest extends PHPUnit_Framework_TestCase {
+
+    private $status;
+
+    public function setUp(){
+        parent::setUp();
+    }
+
+    public function tearDown(){
+        parent::tearDown();
+        unset($this->status);
+    }
+
+    public function testConstruct_createFile_success(){
+        $path = 'temp/writeable/status.json';
+        $this->status = new Status($path);
+        $this->assertTrue(file_exists($path));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testConstruct_createFileNotWriteable(){
+        $path = 'temp/notWriteable/status.json';
+        $this->status = new Status($path);
+        $this->assertFalse(file_exists($path));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testConstruct_fileNotWriteable(){
+        $path = 'temp/notWriteable/status.json';
+        $this->status = new Status($path);
+        $this->assertFalse(file_exists($path));
+    }
+
+    public function testSave_SaveNextWakeup() {
+        $time = time();
+        $path = 'temp/writeable/status.json';
+        $this->status = new Status($path);
+
+        $this->status->nextWakeup = $time;
+        $this->status->save();
+
+        $file = file_get_contents($path);
+        $fileContainsTime = preg_match('/' .$time . '/', $file) ? true : false;
+
+        $this->assertTrue($fileContainsTime);
+    }
+
+    public function testSave_SaveSleepers() {
+        $path = 'temp/writeable/status.json';
+        $this->status = new Status($path);
+
+        $this->status->sleepers = array(
+            'sleeper1' => 'Sleeper One',
+            'sleeper2' => 'Sleeper Two',
+        );
+        $this->status->save();
+
+        $file = file_get_contents($path);
+        $fileContainsSleeper1 = preg_match('/sleeper1/', $file) ? true : false;
+        $fileContainsSleeper2 = preg_match('/sleeper2/', $file) ? true : false;
+
+        $this->assertTrue($fileContainsSleeper2);
+        $this->assertTrue($fileContainsSleeper2);
+    }
+
+    public function testSaveAndLoad(){
+
+        $nextWakeup = time();
+        $sleepers = array(
+            'sleeper1' => 'Sleeper One',
+            'sleeper2' => 'Sleeper Two',
+        );
+
+        $path = 'temp/writeable/status.json';
+        $this->status = new Status($path);
+
+        $this->status->sleepers = $sleepers;
+        $this->status->nextWakeup = $nextWakeup;
+        $this->status->save();
+
+        $this->status = new Status($path);
+        $this->status->load();
+
+        $this->assertEquals($this->status->sleepers, $sleepers);
+        $this->assertEquals($this->status->nextWakeup, $nextWakeup);
+    }
+
+    public function testAutomaticLoad(){
+
+        $nextWakeup = time();
+        $sleepers = array(
+            'sleeper1' => 'Sleeper One',
+            'sleeper2' => 'Sleeper Two',
+        );
+
+        $path = 'temp/writeable/status.json';
+        $this->status = new Status($path);
+        $this->status->sleepers = $sleepers;
+        $this->status->nextWakeup = $nextWakeup;
+        $this->status->save();
+
+        $this->status = new Status($path);
+        $this->assertEquals($this->status->sleepers, $sleepers);
+        $this->assertEquals($this->status->nextWakeup, $nextWakeup);
+    }
+}
